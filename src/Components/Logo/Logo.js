b445d9ae61b7636c0e316d3ecb65df136fb9ba7f
RutamBhagat <rutambhagat@gmail.com>
2022-10-19 10:46:44 +0530

    removed react tilt and bootstap from node modules

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,20 +1,14 @@
 import React from "react";
-import Tilt from "react-tilt";
 import LogoSrc from "./project.png";
 
 const Logo = () => {
   return (
     <div class="w-full">
-      <Tilt
-        class="Tilt rounded-lg drop-shadow-lg flex flex-col justify-center"
-        options={{ max: 10 }}
-      >
-        <img
-          class="object-cover object-center rounded-lg Tilt-inner"
-          alt="hero"
-          src={LogoSrc}
-        />
-      </Tilt>
+      <img
+        class="object-cover object-center rounded-lg Tilt-inner"
+        alt="hero"
+        src={LogoSrc}
+      />
     </div>
   );
 };
